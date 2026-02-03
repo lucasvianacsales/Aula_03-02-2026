21d7c442a737214409a21d02c72ce90edd7c53a1
Lucas <100038556+lucasvianacsales@users.noreply.github.com>
2026-02-03 20:34:25 -0300

    Update pratica.py

--- pratica.py
+++ pratica.py
@@ -5,13 +5,9 @@ dados = pd.read_excel("vendas.xlsx")
 dados["valor_total"] = dados["preco_unitario"]*dados["quantidade"]
 
 total = dados["valor_total"].sum()
-
 media = dados["valor_total"].mean()
-
 mediana = dados["valor_total"].median()
-
 minimo = dados["valor_total"].min()
-
 maximo = dados["valor_total"].max()
 
 print(f"""
@@ -24,6 +20,19 @@ print(f"""
 abaixo_1000 = (dados["valor_total"] < 1000).sum()
 abaixo_1000_per = (dados[dados["valor_total"] < 1000].shape[0] / dados.shape[0])*100
 
-print(f"{abaixo_1000} das vendas foram abaixo de R$ 1.000,00 e isso representa {abaixo_1000_per}% das vendas")
+dados.loc[(dados["valor_total"] < 1000), "categoria"] = "abaixo"
+dados.loc[(dados["valor_total"] >= 1000) & (dados["valor_total"] < 10000), "categoria"] = "intermed"
+dados.loc[(dados["valor_total"] >= 10000), "categoria"] = "acima"
 
-print(dados)
+print(f"{abaixo_1000} vendas foram abaixo de R$ 1.000,00 e isso representa {abaixo_1000_per}% das vendas")
+
+acima = dados[dados["categoria"]=="acima"]
+acima_per = (acima.shape[0] / dados.shape[0])*100
+
+intermed_per = (dados[dados["categoria"]=="intermed"].shape[0] / dados.shape[0])*100
+
+print(f"{acima.shape[0]} vendas foram acima de R$ 10.000,00 e isso representa {acima_per:.2f}% das vendas")
+
+print(f"Vendas entre R$ 1.000,00 e 10.000,00 representam {intermed_per:.1f}% das vendas")
+
+# print(dados.sample(20))
