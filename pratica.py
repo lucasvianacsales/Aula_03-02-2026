c934359d9f11e16215127763b1dee0919ec7a3a0
Lucas <100038556+lucasvianacsales@users.noreply.github.com>
2026-02-04 20:06:02 -0300

    Update pratica.py

--- pratica.py
+++ pratica.py
@@ -4,18 +4,18 @@ dados = pd.read_excel("vendas.xlsx")
 
 dados["valor_venda"] = dados["preco_unitario"]*dados["quantidade"]
 
-total = dados["valor_venda"].sum()
-media = dados["valor_venda"].mean()
-mediana = dados["valor_venda"].median()
-minimo = dados["valor_venda"].min()
-maximo = dados["valor_venda"].max()
+# total_vendas_bruto = dados["valor_venda"].sum()
+# media = dados["valor_venda"].mean()
+# mediana = dados["valor_venda"].median()
+# minimo = dados["valor_venda"].min()
+# maximo = dados["valor_venda"].max()
 
-print(f"""
-    Receita total das vendas: R$ {total}
-    Média das vendas: R$ {media:.2f}
-    Menor venda: R$ {minimo}
-    Maior venda: R$ {maximo}
-""")
+# print(f"""
+#     Receita total das vendas: R$ {total_vendas_bruto}
+#     Média das vendas: R$ {media:.2f}
+#     Menor venda: R$ {minimo}
+#     Maior venda: R$ {maximo}
+# """)
 
 abaixo_1000 = (dados["valor_venda"] < 1000).sum()
 abaixo_1000_per = (dados[dados["valor_venda"] < 1000].shape[0] / dados.shape[0])*100
@@ -44,3 +44,36 @@ dados["valor_liquido"] = (((100-dados["imposto"])*dados["valor_venda"])/100).rou
 
 print(dados[["valor_venda", "imposto", "valor_liquido"]])
 
+dados.loc[(dados["idade"] < 30), "faixa_etaria"] = "Jovem"
+dados.loc[(dados["idade"] >= 30) & (dados["idade"] < 50), "faixa_etaria"] = "Adulto"
+dados.loc[(dados["idade"] >= 50), "faixa_etaria"] = "Sênior"
+
+print(dados[["idade", "faixa_etaria"]].sample(20))
+
+total_vendas_bruto = dados["valor_venda"].sum()
+total_vendas_liquido = dados["valor_liquido"].sum()
+
+total_imposto = ((dados["valor_venda"]*dados["imposto"])/100).sum()
+
+vendedores_unicos = dados["vendedor"].nunique()
+clientes_unicos = dados["cliente"].nunique()
+
+ticket_medio_liquido = dados["valor_liquido"].sum() / clientes_unicos
+
+media = dados["valor_venda"].mean()
+mediana = dados["valor_venda"].median()
+minimo = dados["valor_venda"].min()
+maximo = dados["valor_venda"].max()
+
+print(f"""
+    Receita total de vendas: R$ {total_vendas_bruto}
+    Receita total de vendas sem impostos: R$ {total_vendas_liquido}
+    Total de impostos: R$ {total_imposto:.2f}
+    Número total de vendas: {dados.shape[0]}
+    Média do valor de venda: R$ {media:.2f}
+    Média do valor de venda liquido por cliente: R$ {ticket_medio_liquido:.2f}
+    Menor venda: R$ {minimo}
+    Maior venda: R$ {maximo}
+""")
+
+print(clientes_unicos)
